f6724cf335d86afdb8b004ab2dff6ee3bb16e728
Max Martínez <max.mauriciomartinez@gmail.com>
2026-01-19 00:35:55 +0100

    Create trece.js

---
+++ api/trece.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch('http://181.209.37.74:8001/play/a07b/index.m3u8');
+    const data = await response.text();
+    
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
+    res.setHeader('Cache-Control', 'no-cache');
+    
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send(`Error: ${error.message}`);
+  }
+}
